b6f4c818173001a634b237124c1a3ef74a89b3f9
mgq278 <mgq278@alumni.ku.dk>
2023-03-16 17:00:35 +0100

    Oliver er sej

--- inauguralproject/HouseholdSpecializationModel.py
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -57,7 +57,7 @@ class HouseholdSpecializationModelClass:
         if par.sigma==1:
             H = HM**(1-par.alpha)*HF**par.alpha
         else :
-            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**((par.sigma)/par.sigma-1)
+            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**((par.sigma)/(par.sigma-1))
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
         utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
@@ -110,7 +110,7 @@ class HouseholdSpecializationModelClass:
 
     def solve(self,do_print=False):
         """ solve model continously """
-
+        x_con=np.linspace(0,24,1000)
         pass    
 
     def solve_wF_vec(self,discrete=False):
